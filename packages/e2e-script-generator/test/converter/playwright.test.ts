0df743515f43ed56c1c6501c871fbb58e36f0f43
Pier Luigi <pierluigi.gudini@gmail.com>
2022-03-12 16:45:05 +0100

    Deleted files

--- packages/e2e-script-generator/test/converter/playwright.test.ts
+++
@@ -1,6 +0,0 @@
-import {PlaywrightE2eScriptGenerator} from "../../src/converter/playwright-e2e-script-converter";
-
-test("test simple conversion", async () => {
-    const result = new PlaywrightE2eScriptGenerator().convert([{name: "click", type: "cookie", timestamp: new Date().getTime()}])
-    expect(result).toBe("1")
-})
