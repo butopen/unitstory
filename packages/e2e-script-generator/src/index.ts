2851ee5b4061b8e27da79f1eaa3a87b06e544789
Pier Luigi <pierluigi.gudini@gmail.com>
2022-03-12 16:41:31 +0100

    Added a simple script to generate a first playwright script

--- packages/e2e-script-generator/src/index.ts
+++ packages/e2e-script-generator/src/index.ts
@@ -1,4 +1,17 @@
-import {PlaywrightE2eScriptGenerator} from "./converter/playwright-e2e-script-converter";
+import {EventsConverter} from "./converter/events-converter";
+import {BLSessionEvent} from "@butopen/user-events-model";
 
-const result = new PlaywrightE2eScriptGenerator().convert([])
-console.log(result)
+(async () => {
+    const event = {
+        "name": "session-start",
+        "type": "session",
+        "timestamp": 1646910428051,
+        "sid": 1646908359206,
+        "url": "https://www.demoblaze.com/index.html",
+        "tab": 1646910416001
+    } as BLSessionEvent
+    const converter = new EventsConverter();
+    const session = converter.convert([event])
+    await session.toPlaywrightScript(false, 100)
+    console.log('Playwright script created')
+})()
